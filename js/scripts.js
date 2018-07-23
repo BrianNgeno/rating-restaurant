25647d0ce2851b8c55da1faf71e606b3be3de596
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-07-23 15:29:47 +0300

    added a function that captures the inputs from the form, added onclick to the submit input

--- js/scripts.js
+++ js/scripts.js
@@ -5,38 +5,15 @@ $(document).ready(function() {
     var lastName =$("input#lastName").val();
     var priority =$("input#priority").val();
     var subject =$("input#subject");
-    var result = parseInt(number);
 
     });
   });
-});
-
-
-
-// $(document).ready(function() {
-//   $("form").click(function(event) {
-//     event.preventDefault();
-//     var number =$("input#number").val();
-//     var result = parseInt(number);
-//     map(result);
-//     sam.forEach(function(result) {
-//       $("#ordered-list").append("<li>" + result + "</li>");
-//     });
-//   });
-// });
-//
-// var sam= [];
-//
-// function map(result) {
-//   for (index = 1; index <= result; index++) {
-//     if (index % 3 === 0){
-//     sam.push("Ping");
-//   } else if (index % 5 === 0){
-//   sam.push("Pong");
-// } else if (index % 15 === 0){
-//   sam.push("Ping Pong");
-// } else {
-//   sam.push(index);
-// }
-// }
-// }
+function myFunction () {
+  var formOutPut = document.getElementById("contactForm");
+    var outPut = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        outPut += x.elements[i].value + "<br>";
+    }
+    document.getElementById("summary").innerHTML = outPut;
+}
