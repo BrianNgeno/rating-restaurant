3160644f9064158601c1b9ea521c3e10034c277e
brianngeno <bkn.ngeno@gmail.com>
2018-07-24 13:05:39 +0300

    added a login button to the home page

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,22 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+$(document).ready(function(){
+  $("button#login").click(function(event){
+    event.preventDefault();
+    var user=$("#input1").val();
+    alert(user+ "YOU ARE LOGGED IN");
+    $("#id01").hide();
+    window.location.reload();
+    });
+});
