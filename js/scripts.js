bfc6f60999d175a550705d6f205a992586a85ada
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-07-24 12:10:11 +0300

    changed the js from .append to .text, added the priority level variables, improved the styling

--- js/scripts.js
+++ js/scripts.js
@@ -10,19 +10,20 @@ $(document).ready(function() {
     event.preventDefault();
     var firstName =$("input#firstName").val();
     var lastName =$("input#lastName").val();
-    // var high =$("input#high").val();
-    // var moderate =$("input#moderate").val();
-    // var low =$("input#low").val();
+    var high =$("input#high").val();
+    var moderate =$("input#moderate").val();
+    var low =$("input#low").val();
     var Subject =$("input#subject").val();
     var priority =$("input#priority").val();
     var newContactForm = new ContactForm(firstName, lastName, priority, subject);
 
-    $("p#summary").append("<p><span class='contain'>" + newContactForm.contactForm + "</span></p>");
+    $("#summary").text(newContactForm);
 
     $("input#firstName").val("");
       $("input#lastName").val("");
       $("input#priority").val("");
       $("input#subject").val("");
+
 
     });
   });
