ad1882da0181c5fec211a3c6f16d7096ba6ee43f
chrikin1 <chrisianbartoo@gmail.com>
2018-07-25 11:43:09 +0300

    modified the star rate and improved on the html

--- js/rate.js
+++ js/rate.js
@@ -5,3 +5,18 @@ function openNav() {
 function closeNav() {
    document.getElementById("myNav").style.width = "0%";
 }
+$(function() {
+         $('#example').barrating({
+           theme: 'fontawesome-stars-o',
+           //readonly: true,
+          // showSelectedRating:false
+         });
+      });
+
+      $(function() {
+          $('#example').barrating({
+            theme: 'fontawesome-stars-o',
+            //readonly: true,
+           // showSelectedRating:false
+          });
+       });
